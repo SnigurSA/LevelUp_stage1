f547ae24b684ed5420db6c0f834d55128fdd1842
SnigurSA <bercut-s-47@mail.ru>
2022-02-06 23:44:38 +0300

    добавил коментарий и переписал строку ввода

--- dz2/dz2_1/dz2_1/dz2-1.cpp
+++ dz2/dz2_1/dz2_1/dz2-1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 double y, x, x1;
 int main()
 {
-    while(1)
+    while(1) //цикл для автозапуска вычисления
     {
-    printf("input x=");
+    printf("Enter x=");
       scanf("%lf", &x);
-    x1 = x * M_PI/180;
+    x1 = x * M_PI / 180;
     y = (2 * cos(x1 - M_PI / 6) + sqrt(2)) / (1 / 2 / log(x1) + pow(sin(pow(x1,2)),2)) * exp(3 * x1);
     printf(" y = %lf\n ", y);
     //return(0);
