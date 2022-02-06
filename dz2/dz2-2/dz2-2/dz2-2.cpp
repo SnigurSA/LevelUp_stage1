7e5c7103a0e4e71a7554e1eb4e4b8abeb63527a6
SnigurSA <bercut-s-47@mail.ru>
2022-02-07 00:07:00 +0300

    применил тернальные операторы пи как внутренняя константа Си

--- dz2/dz2-2/dz2-2/dz2-2.cpp
+++ dz2/dz2-2/dz2-2/dz2-2.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
 #include <math.h> // подключаем math.h
-using namespace std;
-double R, a, d, pi = 3.1415926, SR, Sa, Sd;
+//using namespace std; отключил элементы С++
+double R, a, d, SR, Sa, Sd;
 int main()
 {
     while (1)
 
 {
-  printf("input radius kruga R=");
+  printf("Vvedite radius kruga R=");
   scanf("%lf", &R);
 
-  printf("input dlinu storony treugolnika a=");
+  printf("Vvedite dlinu storony treugolnika a=");
   scanf("%lf", &a);
 
-  printf("input dlinu storony kvadrata d=");
+  printf("Vvedite dlinu storony kvadrata d=");
   scanf("%lf", &d);
 
-  SR = pow(R,2) * pi;
+  SR = pow(R,2) * M_PI;
   Sa = pow(a,2) * sqrt(3) / 4;
   Sd = pow(d,2);
   printf(" SR = %lf Sa = %lf Sd = %lf\n ", SR, Sa, Sd);
-  if (SR > Sa)
-  {
-      if (SR > Sd)
-      {
-          printf("Ploshad kruga naibolshaya");
-      }
-      else
-      {
-          printf("Ploshad kvadrata naibolshaya");
-      }
-  }
-  else
-    {
-      if (Sa > Sd)
-       {
-          printf("Ploshad treugolnika naibolshaya");
-       }
-      else
-      {
-          printf("Ploshad kvadrata naibolshaya       ");
-      }
-  }
-
-}
+  SR > Sa ? (SR > Sd ? printf("Ploshad kruga naibolshaya \n ") : printf("Ploshad kvadrata naibolshaya \n "))
+          : (Sa > Sd ? printf("Ploshad treugolnika naibolshaya \n ") : printf("Ploshad kvadrata naibolshaya \n "));
+ }
 }
 
