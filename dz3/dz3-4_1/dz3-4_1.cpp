60bfac0c987ec47acc2adf0500bf58068dcacb80
SnigurSA <bercut-s-47@mail.ru>
2022-03-04 17:34:35 +0300

    переписал код через оператор сдвига влево

--- dz3/dz3-4_1/dz3-4_1.cpp
+++ dz3/dz3-4_1/dz3-4_1.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
-
+#include <math.h>
 int main ()
 {
-long long int i, N, flag1 = 0;
+long long int i, N;
+bool flag1 = 0;
 
     printf("Vvedite chilo N= ");
     scanf("%lld", &N);
     // printf("%lld", N);
     i = 1; // записываем "1" в нулевой бит переменной i
-    i <<= 62; // сдвигаем "1" на 62 позиции влево, при сдвиге на 63 получается бесконечный цикл вывода "1" или не считает вообще
 
-    //printf ("%lld", i);
+    //printf ("%lld", abs(i));
     printf("V dvoichnom formate N= ");
 
 
-    while (i > 0)
+    for (int r = 63; r > 0; r--)
      {
-       if (N & i)        // битовое И - в данном случае вернет 2^i - если i-ый бит 1
+       i <<= r;
+        if (N & i)        // битовое И - в данном случае вернет 2^i - если i-ый бит 1
           {
              printf("1");
              flag1 = 1;
+
           }
        else
-           if (flag1 == 1) // чтобыы не печатать лишние нули слева
+           if (flag1) // чтобыы не печатать лишние нули слева
               printf("0");
-       i >>= 1;         // битовый сдвиг, запись числа смещается на один бит
+       i = 1;
      }
 
 }
