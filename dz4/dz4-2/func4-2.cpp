1e010c8a2958a05459f136fa867b50c10ff88d1b
SnigurSA <bercut-s-47@mail.ru>
2022-03-13 00:05:54 +0300

    исправил код по замечанию Сложная запись какая то for(int j=0; i < size2 && temp; ++j) temp = B[j] == A[i]; в этой строке исправил условие на: temp = B[j] != A[i];

--- dz4/dz4-2/func4-2.cpp
+++ dz4/dz4-2/func4-2.cpp
@@ -7,17 +7,19 @@ void func4_2(int mass1[], int size1, int mass2[], int size2, int mass3[], int *s
     for(int i = 0; i < size1; ++i)
     {
         temp = true;
+/*
         for(int j=0; j < size2; ++j)
         {
-
-
             if (mass1[i] == mass2[j])
             {
                 temp = false;
                 break;
             }
-
         }
+*/
+        for(int j=0; (j < size2) && temp; ++j)
+        temp = mass1[i] != mass2[j];
+
         if (temp)
         {
             mass3[k] = i;
